824eb842ebc04049c83a00dba4d6c03ed404b8d5
Peter LaValle <peter.lavalle@gmail.com>
2025-07-30 12:38:43 +0100

    scalafmt

--- src/main/scala/peterlavalle/eJSON.scala
+++ src/main/scala/peterlavalle/eJSON.scala
@@ -7,238 +7,237 @@ import scala.math.BigDecimal.javaBigDecimal2bigDecimal
 import scala.util.{Failure, Try}
 
 object eJSON {
-	given F[String] =
-		field.pure {
-			(o, k) =>
-				o.get(k).toString
-		}
+  given F[String] =
+    field.pure { (o, k) =>
+      o.get(k).toString
+    }
 
-	given F[Int] =
-		field.pure {
-			(o: JSONObject, k: String) =>
-				o.get(k) match
-					case i: Int =>
-						i
-					case s: String =>
-						s.toInt
-		}
+  given F[Int] =
+    field.pure { (o: JSONObject, k: String) =>
+      o.get(k) match
+        case i: Int =>
+          i
+        case s: String =>
+          s.toInt
+    }
 
-	given F[Float] =
-		field.pure {
-			(o, k) =>
-				o.get(k) match
-					case i: Int =>
-						i.toFloat
-					case s: String =>
-						s.toFloat
-					case f: Float =>
-						f
-					case d: Double => d.toFloat
-					case b: java.math.BigDecimal =>
-						b.toFloat
-		}
+  given F[Float] =
+    field.pure { (o, k) =>
+      o.get(k) match
+        case i: Int =>
+          i.toFloat
+        case s: String =>
+          s.toFloat
+        case f: Float =>
+          f
+        case d: Double               => d.toFloat
+        case b: java.math.BigDecimal =>
+          b.toFloat
+    }
 
-	given F[File] = field.pure((o: JSONObject, k: String) => File(o.getString(k)).getAbsoluteFile)
+  given F[File] = field.pure((o: JSONObject, k: String) =>
+    File(o.getString(k)).getAbsoluteFile
+  )
 
-	given F[JSONObject] = field.pure(_ getJSONObject _)
+  given F[JSONObject] = field.pure(_ getJSONObject _)
 
-	extension [W](seq: Iterable[W])
-		def toJSONArray(set: (JSONArray, W) => Unit): JSONArray =
-			val json = JSONArray()
-			seq.foreach((item: W) => set(json, item))
-			json
+  extension [W](seq: Iterable[W])
+    def toJSONArray(set: (JSONArray, W) => Unit): JSONArray =
+      val json = JSONArray()
+      seq.foreach((item: W) => set(json, item))
+      json
 
-	extension (a: JSONArray)
-		def toListOf[E: F]: Try[List[E]] =
-			(0 until a.length())
-				.foldLeft(Try(List[E]())) {
-					case (left, index) =>
-						left.flatMap {
-							left =>
-								summon[F[E]].onArray(a, index).map(left :+ _)
-						}
-				}
-		def asStrings: Seq[String] = a.toListOf[String].get
-		def asObjects: Seq[JSONObject] =
-			(0 until a.length())
-				.to(LazyList)
-				.map(a.getJSONObject)
+  extension (a: JSONArray)
+    def toListOf[E: F]: Try[List[E]] =
+      (0 until a.length())
+        .foldLeft(Try(List[E]())) { case (left, index) =>
+          left.flatMap { left =>
+            summon[F[E]].onArray(a, index).map(left :+ _)
+          }
+        }
+    def asStrings: Seq[String] = a.toListOf[String].get
+    def asObjects: Seq[JSONObject] =
+      (0 until a.length())
+        .to(LazyList)
+        .map(a.getJSONObject)
 
-	def field[I: F]: field0[I] = field0[I]()
+  def field[I: F]: field0[I] = field0[I]()
 
-	def flag(s: String): field0[Unit] =
-		given F[Unit] =
-			field.bind {
-				(j: JSONObject, k: String) =>
-					val v = j.getString(k)
-					if (v != s)
-						Failure(
-							Exception(
-								s"its not a match - `$s` != `$v`"
-							)
-						)
-					else
-						Try(())
-			}
+  def flag(s: String): field0[Unit] =
+    given F[Unit] =
+      field.bind { (j: JSONObject, k: String) =>
+        val v = j.getString(k)
+        if (v != s)
+          Failure(
+            Exception(
+              s"its not a match - `$s` != `$v`"
+            )
+          )
+        else
+          Try(())
+      }
 
-		field0[Unit]()
+    field0[Unit]()
 
-	def array[I: F]: field0[List[I]] =
-		type Q = List[I]
+  def array[I: F]: field0[List[I]] =
+    type Q = List[I]
 
-		field[Q]
+    field[Q]
 
-	def setOf[I: F]: field0[Set[I]] =
-		sys.error("wrap the array one")
+  def setOf[I: F]: field0[Set[I]] =
+    sys.error("wrap the array one")
 
-	trait E[Q] extends F[Q] {
+  trait E[Q] extends F[Q] {
 
-		def ![V](f: Q => V): E[V] =
-			apply(_: JSONObject)
-				.map(f)
+    def ![V](f: Q => V): E[V] =
+      apply(_: JSONObject)
+        .map(f)
 
-		def |[V >: Q, Z <: V](them: E[Z]): E[V] =
-			(json: JSONObject) =>
-				apply(json).orElse(them(json))
+    def |[V >: Q, Z <: V](them: E[Z]): E[V] =
+      (json: JSONObject) => apply(json).orElse(them(json))
 
-		def unapply(src: String): Option[Q] = unapply(JSONObject(JSONTokener(src)))
+    def unapply(src: String): Option[Q] = unapply(JSONObject(JSONTokener(src)))
 
-		def unapply(o: JSONObject): Option[Q] = apply(o).toOption
+    def unapply(o: JSONObject): Option[Q] = apply(o).toOption
 
-		def apply(o: JSONObject): Try[Q]
+    def apply(o: JSONObject): Try[Q]
 
-		override def onObject(json: JSONObject, key: String): Try[Q] =
-			apply(json.getJSONObject(key))
-	}
+    override def onObject(json: JSONObject, key: String): Try[Q] =
+      apply(json.getJSONObject(key))
+  }
 
-	trait F[Q] {
-		def onArray(json: JSONArray, i: Int): Try[Q] =
+  trait F[Q] {
+    def onArray(json: JSONArray, i: Int): Try[Q] =
 
-			if (i < 0 || json.length() <= i)
-				Failure(
-					IndexOutOfBoundsException(s"index $i is OOB in array $json")
-				)
-			else
-				val n = getClass.getSimpleName
-				onObject(
-					new JSONObject().put(n, json.get(i)),
-					n
-				)
+      if (i < 0 || json.length() <= i)
+        Failure(
+          IndexOutOfBoundsException(s"index $i is OOB in array $json")
+        )
+      else
+        val n = getClass.getSimpleName
+        onObject(
+          new JSONObject().put(n, json.get(i)),
+          n
+        )
 
-		def onObject(json: JSONObject, key: String): Try[Q]
-	}
+    def onObject(json: JSONObject, key: String): Try[Q]
+  }
 
-	extension (s: String)
-		def /[Q](f: E[Q]): E[Q] =
+  extension (s: String)
+    def /[Q](f: E[Q]): E[Q] =
 
-			(json: JSONObject) =>
-				val keys = json.keySet()
-				val value =
-					if (1 != keys.size())
-						None
-					else {
-						val key = keys.iterator().next()
-						if (key != s)
-							None
-						else
-							json.optJSONObject(key) match
-								case null =>
-									None
-								case json =>
-									f.unapply(json)
-					}
+      (json: JSONObject) =>
+        val keys = json.keySet()
+        val value =
+          if (1 != keys.size())
+            None
+          else {
+            val key = keys.iterator().next()
+            if (key != s)
+              None
+            else
+              json.optJSONObject(key) match
+                case null =>
+                  None
+                case json =>
+                  f.unapply(json)
+          }
 
-				Try {
-					value.get
-				}
+        Try {
+          value.get
+        }
 
-	final class field0[I: F]():
-		inline def flatMap[O](inline func: I => E[O]): E[O] =
-			${ field1.code('{ bind(func) }, '{ func }) }
+  final class field0[I: F]():
+    inline def flatMap[O](inline func: I => E[O]): E[O] =
+      ${ field1.code('{ bind(func) }, '{ func }) }
 
-		private def bind[O](get: I => E[O]): String => E[O] =
-			(k: String) =>
-				(o: JSONObject) =>
-					if (!o.has(k))
-						Failure(KeyMissing(s"key $k is not in $o"))
-					else
-						summon[F[I]]
-							.onObject(o, k)
-							.map(get)
-							.flatMap(_.apply(o))
+    private def bind[O](get: I => E[O]): String => E[O] =
+      (k: String) =>
+        (o: JSONObject) =>
+          if (!o.has(k))
+            Failure(KeyMissing(s"key $k is not in $o"))
+          else
+            summon[F[I]]
+              .onObject(o, k)
+              .map(get)
+              .flatMap(_.apply(o))
 
-		inline def map[O](inline func: I => O): E[O] =
-			${ field1.code('{ pure(func) }, '{ func }) }
+    inline def map[O](inline func: I => O): E[O] =
+      ${ field1.code('{ pure(func) }, '{ func }) }
 
-		private def pure[O](get: I => O): String => E[O] =
-			(k: String) =>
-				(o: JSONObject) =>
-					if (!o.has(k))
-						Failure(
-							IndexOutOfBoundsException(s"key $k is not in $o")
-						)
-					else
-						summon[F[I]]
-							.onObject(o, k)
-							.map(get)
+    private def pure[O](get: I => O): String => E[O] =
+      (k: String) =>
+        (o: JSONObject) =>
+          if (!o.has(k))
+            Failure(
+              IndexOutOfBoundsException(s"key $k is not in $o")
+            )
+          else
+            summon[F[I]]
+              .onObject(o, k)
+              .map(get)
 
-	private case class KeyMissing(message: String) extends Exception(message)
+  private case class KeyMissing(message: String) extends Exception(message)
 
+  object field {
+    def read[Q](key: String => Boolean)(value: String => Q): F[Q] =
+      pure { (json: JSONObject, name: String) =>
+        require(
+          json.has(name)
+        )
+        val text = json.getString(name)
+        require(
+          key(text)
+        )
+        value(text)
+      }
 
-	object field {
-		def read[Q](key: String => Boolean)(value: String => Q): F[Q] =
-			pure {
-				(json: JSONObject, name: String) =>
-					require(
-						json.has(name)
-					)
-					val text = json.getString(name)
-					require(
-						key(text)
-					)
-					value(text)
-			}
+    def pure[Q](get: (JSONObject, String) => Q): F[Q] =
+      (json: JSONObject, key: String) =>
+        try Try(get(json, key))
+        catch
+          case e: Throwable =>
+            Failure(e)
 
-		def pure[Q](get: (JSONObject, String) => Q): F[Q] =
-			(json: JSONObject, key: String) =>
-				try
-					Try(get(json, key))
-				catch
-					case e: Throwable =>
-						Failure(e)
+    def bind[Q](get: (JSONObject, String) => Try[Q]): F[Q] =
+      (json: JSONObject, key: String) => get(json, key)
+  }
 
-		def bind[Q](get: (JSONObject, String) => Try[Q]): F[Q] =
-			(json: JSONObject, key: String) =>
-				get(json, key)
-	}
+  private object field1:
 
-	private object field1:
+    import scala.quoted.*
 
-		import scala.quoted.*
+    def code[I: Type, O: Type](
+        q: Expr[String => E[O]],
+        f: Expr[I => Any]
+    )(using quotes: Quotes): Expr[E[O]] =
+      import quotes.reflect.*
 
-		def code[I: Type, O: Type]
-		(
-			q: Expr[String => E[O]],
-			f: Expr[I => Any]
-		)(using quotes: Quotes): Expr[E[O]] =
-			import quotes.reflect.*
+      // Inspect the lambda to extract parameter name
+      f.asTerm match {
 
-			// Inspect the lambda to extract parameter name
-			f.asTerm match {
+        case Inlined(
+              _,
+              List(),
+              Block(
+                List(DefDef(_, List(List(ValDef(name: String, _, _))), _, _)),
+                _
+              )
+            ) =>
+          // pass the name into the whatnot
+          val key: Expr[String] = Expr(name)
+          '{ $q($key) }
 
-				case Inlined(_, List(), Block(List(DefDef(_, List(List(ValDef(name: String, _, _))), _, _)), _)) =>
-					// pass the name into the whatnot
-					val key: Expr[String] = Expr(name)
-					'{ $q($key) }
+        case _ =>
+          quotes.reflect.report.error(
+            s"pure expected a for{}yield comprehension"
+          )
+          '{ ??? }
+      }
 
-				case _ =>
-					quotes.reflect.report.error(s"pure expected a for{}yield comprehension")
-					'{ ??? }
-			}
-
-	given [T: F]: F[List[T]] with {
-		override def onObject(json: JSONObject, key: String): Try[List[T]] =
-			json.getJSONArray(key).toListOf[T]
-	}
+  given [T: F]: F[List[T]] with {
+    override def onObject(json: JSONObject, key: String): Try[List[T]] =
+      json.getJSONArray(key).toListOf[T]
+  }
 
 }
--- src/test/scala/peterlavalle/eJSONTest.scala
+++ src/test/scala/peterlavalle/eJSONTest.scala
@@ -7,236 +7,229 @@ import scala.util.{Failure, Success, Try}
 
 class eJSONTest extends munit.FunSuite {
 
-	test("do the thing - but - with the optionals") {
-		import eJSON.*
+  test("do the thing - but - with the optionals") {
+    import eJSON.*
 
-		case class FooBar(i: Int, s: String)
+    case class FooBar(i: Int, s: String)
 
-		val foobar: E[FooBar] =
-			for {
-				i <- field[Int]
-				s <- field[String]
-			} yield {
-				FooBar(i, s)
-			}
+    val foobar: E[FooBar] =
+      for {
+        i <- field[Int]
+        s <- field[String]
+      } yield {
+        FooBar(i, s)
+      }
 
-		assertEquals(
-			foobar(
-				new JSONObject()
-					.put("i", "7")
-					.put("s", -9)
-			).get,
-			FooBar(7, "-9")
-		)
-	}
+    assertEquals(
+      foobar(
+        new JSONObject()
+          .put("i", "7")
+          .put("s", -9)
+      ).get,
+      FooBar(7, "-9")
+    )
+  }
 
-	test("test one hot") {
-		case class OneHo(i: Int)
-		import eJSON.*
+  test("test one hot") {
+    case class OneHo(i: Int)
+    import eJSON.*
 
-		val goo: E[OneHo] =
-			"foo" / {
-				for {
-					g <- field[Int]
-				} yield {
-					OneHo(g)
-				}
-			}
+    val goo: E[OneHo] =
+      "foo" / {
+        for {
+          g <- field[Int]
+        } yield {
+          OneHo(g)
+        }
+      }
 
+    "{foo:{g:9}}" match
+      case goo(OneHo(q)) =>
+        assertEquals(q, 9)
 
-		"{foo:{g:9}}" match
-			case goo(OneHo(q)) =>
-				assertEquals(q, 9)
+    assertEquals(
+      goo.unapply("{bar:{g:19}}"),
+      None
+    )
 
-		assertEquals(
-			goo.unapply("{bar:{g:19}}"),
-			None
-		)
+    assertEquals(
+      goo.unapply("{foo:{G:9}}"),
+      None
+    )
+  }
 
-		assertEquals(
-			goo.unapply("{foo:{G:9}}"),
-			None
-		)
-	}
+  test("test two one hots") {
+    import eJSON.*
 
-	test("test two one hots") {
-		import eJSON.*
+    case class Box(i: Int, s: Int)
 
-		case class Box(i: Int, s: Int)
+    val foo = "foo" / {
+      for {
+        g <- field[Int]
+      } yield {
+        Box(g, 1)
+      }
+    }
 
-		val foo = "foo" / {
-			for {
-				g <- field[Int]
-			} yield {
-				Box(g, 1)
-			}
-		}
+    val bar = "bar" / {
+      for {
+        g <- field[Int]
+      } yield {
+        Box(g, 2)
+      }
+    }
 
-		val bar = "bar" / {
-			for {
-				g <- field[Int]
-			} yield {
-				Box(g, 2)
-			}
-		}
+    def go(src: String) =
+      src match
+        case foo(v) =>
+          v
+        case bar(v) =>
+          v
+        case _ =>
+          null
 
-		def go(src: String) =
-			src match
-				case foo(v) =>
-					v
-				case bar(v) =>
-					v
-				case _ =>
-					null
+    assertEquals(
+      go("{foo:{G:9}}"),
+      null
+    )
+    assertEquals(
+      go("{bar:{g:19}}"),
+      Box(19, 2)
+    )
+    assertEquals(
+      go("{foo:{g:'-179'}}"),
+      Box(-179, 1)
+    )
+  }
 
+  test("test one-hot alt-merge") {
+    import eJSON.*
 
-		assertEquals(
-			go("{foo:{G:9}}"),
-			null
-		)
-		assertEquals(
-			go("{bar:{g:19}}"),
-			Box(19, 2)
-		)
-		assertEquals(
-			go("{foo:{g:'-179'}}"),
-			Box(-179, 1)
-		)
-	}
+    case class Box(i: Int, s: Int)
 
-	test("test one-hot alt-merge") {
-		import eJSON.*
+    val foo = "foo" / {
+      for {
+        g <- field[Int]
+      } yield {
+        Box(g, 1)
+      }
+    }
 
-		case class Box(i: Int, s: Int)
+    val bar = "bar" / {
+      for {
+        g <- field[Int]
+      } yield {
+        Box(g, 2)
+      }
+    }
 
-		val foo = "foo" / {
-			for {
-				g <- field[Int]
-			} yield {
-				Box(g, 1)
-			}
-		}
+    val goo = foo | bar
 
-		val bar = "bar" / {
-			for {
-				g <- field[Int]
-			} yield {
-				Box(g, 2)
-			}
-		}
+    def go(src: String) =
+      src match
+        case goo(v) =>
+          v
+        case _ =>
+          null
 
-		val goo = foo | bar
+    assertEquals(
+      go("{foo:{G:9}}"),
+      null
+    )
+    assertEquals(
+      go("{bar:{g:19}}"),
+      Box(19, 2)
+    )
+    assertEquals(
+      go("{foo:{g:'-179'}}"),
+      Box(-179, 1)
+    )
+  }
 
-		def go(src: String) =
-			src match
-				case goo(v) =>
-					v
-				case _ =>
-					null
+  test("test toListOf on floats") {
+    val src = "[1, '3.4', -5.0]"
+    val actual: List[Float] = JSONArray(src).toListOf[Float].get
 
-		assertEquals(
-			go("{foo:{G:9}}"),
-			null
-		)
-		assertEquals(
-			go("{bar:{g:19}}"),
-			Box(19, 2)
-		)
-		assertEquals(
-			go("{foo:{g:'-179'}}"),
-			Box(-179, 1)
-		)
-	}
+    assertEquals(
+      actual,
+      List(1.0f, 3.4f, -5.0f)
+    )
+  }
 
-	test("test toListOf on floats") {
-		val src = "[1, '3.4', -5.0]"
-		val actual: List[Float] = JSONArray(src).toListOf[Float].get
+  test("test simple") {
 
-		assertEquals(
-			actual,
-			List(1.0f, 3.4f, -5.0f)
-		)
-	}
+    class Foo(val a: Int)
 
-	test("test simple") {
+    val f: E[Foo] =
+      for {
+        a <- field[Int]
+      } yield {
+        Foo(a)
+      }
 
-		class Foo(val a: Int)
+    val r: Try[Foo] =
+      f(JSONObject().put("a", 12))
 
-		val f: E[Foo] =
-			for {
-				a <- field[Int]
-			} yield {
-				Foo(a)
-			}
+    assertEquals(r.get.a, 12)
+  }
 
-		val r: Try[Foo] =
-			f(JSONObject().put("a", 12))
+  test("test foo|bar") {
 
-		assertEquals(r.get.a, 12)
-	}
+    class Foo(val a: Int)
+    class Bar(val c: Float)
 
-	test("test foo|bar") {
+    val p: E[Foo | Bar] =
+      val f: E[Foo] =
+        for {
+          a <- field[Int]
+        } yield {
+          Foo(a)
+        }
+      val b: E[Bar] =
+        for {
+          boo <- field[Float]
+        } yield {
+          Bar(boo * -1)
+        }
 
+      f | b
 
-		class Foo(val a: Int)
-		class Bar(val c: Float)
+    p(JSONObject().put("a", 13)) match
+      case Failure(exception)  => throw exception
+      case Success(value: Foo) =>
+        assert(value.a == 13)
+      case what =>
+        fail(what.toString)
 
-		val p: E[Foo | Bar] =
-			val f: E[Foo] =
-				for {
-					a <- field[Int]
-				} yield {
-					Foo(a)
-				}
-			val b: E[Bar] =
-				for {
-					boo <- field[Float]
-				} yield {
-					Bar(boo * -1)
-				}
+    p(JSONObject().put("boo", 1.2f)) match
+      case Failure(exception)  => throw exception
+      case Success(value: Bar) =>
+        assert(value.c == -1.2f)
+      case what =>
+        fail(what.toString)
+  }
 
-			f | b
+  test("test map") {
+    case class Foo(i: Int)
+    case class Bar(s: String)
 
+    val f =
+      for {
+        i <- field[Int]
+      } yield {
+        Foo(i)
+      }
+    val b =
+      f ! { (f) =>
+        Bar(f.toString)
+      }
 
-		p(JSONObject().put("a", 13)) match
-			case Failure(exception) => throw exception
-			case Success(value: Foo) =>
-				assert(value.a == 13)
-			case what =>
-				fail(what.toString)
+    assertEquals(
+      b(JSONObject().put("i", 27)),
+      Success(
+        Bar("Foo(27)")
+      )
+    )
 
-		p(JSONObject().put("boo", 1.2f)) match
-			case Failure(exception) => throw exception
-			case Success(value: Bar) =>
-				assert(value.c == -1.2f)
-			case what =>
-				fail(what.toString)
-	}
-
-
-
-	test("test map") {
-		case class Foo(i: Int)
-		case class Bar(s: String)
-
-		val f =
-			for {
-				i <- field[Int]
-			} yield {
-				Foo(i)
-			}
-		val b =
-			f ! {
-				(f) =>
-					Bar(f.toString)
-			}
-
-		assertEquals(
-			b(JSONObject().put("i", 27)),
-			Success(
-				Bar("Foo(27)")
-			)
-		)
-
-	}
+  }
 }
